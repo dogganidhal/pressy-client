3805cae150c4a7c60ee81c0e26a26c546de6226e
DOGGA Nidhal <dogga.nidhal@gmail.com>
2019-06-13 13:26:30 +0200

    Fix Android PDF dependency

--- lib/widgets/settings/terms_of_use/terms_of_use_widget.dart
+++ lib/widgets/settings/terms_of_use/terms_of_use_widget.dart
@@ -1,24 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pressy_client/utils/style/app_theme.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 
 class TermsOfUseWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: ColorPalette.orange),
-        title: Text("CGU"),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        elevation: 1,
-      ),
-      url: "https://srv-file1.gofile.io/download/yxrH1b/condition-general.pdf",
-      withZoom: true
-    );
-    // return Scaffold(
+    // return WebviewScaffold(
     //   appBar: AppBar(
     //     iconTheme: IconThemeData(color: ColorPalette.orange),
     //     title: Text("CGU"),
@@ -26,11 +15,22 @@ class TermsOfUseWidget extends StatelessWidget {
     //     centerTitle: true,
     //     elevation: 1,
     //   ),
-    //   body: SingleChildScrollView(
-    //     padding: EdgeInsets.all(12),
-    //     child: WebViewScaffold
-    //   ),
+    //   url: "https://srv-file1.gofile.io/download/yxrH1b/condition-general.pdf",
+    //   withZoom: true
     // );
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: ColorPalette.orange),
+        title: Text("CGU"),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: WebView(
+        initialUrl: "https://drive.google.com/file/d/1f_KrL3bRhlIQ7kL2JrcZ2ZhmZyCxxVkL/view",
+        javascriptMode: JavascriptMode.unrestricted,
+      )
+    );
   }
 
 }
